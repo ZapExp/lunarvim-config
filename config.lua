a412d1892422df9dd647f35d1431a60cf1fca822
David <zapeexp@gmail.com>
2023-05-04 16:04:08 -0500

    change hop keys

--- config.lua
+++ config.lua
@@ -251,7 +251,8 @@ lvim.plugins = {
 		branch = "v2", -- optional but strongly recommended
 		config = function()
 			-- you can configure Hop the way you like here; see :h hop-config
-			require("hop").setup({ keys = "etovxqpdygfblzhckisuran" })
+			-- require("hop").setup({ keys = "etovxqpdygfblzhckisuran" })
+			require("hop").setup({ keys = "asdfghjklñ" })
 		end,
 	},
 	{
